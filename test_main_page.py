7c02ff5502881c5d642a7f89292b7acc0dcf8d4e
Efimovnik0707 <tomatik0707@gmail.com>
2022-05-25 17:00:31 +0200

    edit test_guest... and added login page test

--- test_main_page.py
+++ test_main_page.py
@@ -5,13 +5,10 @@ from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
      link = "http://selenium1py.pythonanywhere.com/"
-     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-     page.open()                      # открываем страницу
-     page.go_to_login_page()          # ищем кнопку логина и кликаем по ней
+     page = MainPage(browser, link)   
+     page.open()                      
+     page.go_to_login_page()          
+     login_page = LoginPage(browser, browser.current_url) 
+     login_page.should_be_login_page() 
+        
     
-    
-def test_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page = LoginPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-    page.open ()                      # открываем страницу
-    page.should_be_login_page()       # запускаем тест на поиск формы логина, регистрации и проверки login в url
